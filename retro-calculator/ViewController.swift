58273544a7f177abc8d83e1f93342b98d714c655
Matt Linaberry <matt.linaberry@mrs-electronic.com>
2016-05-20 17:01:44 -0400

    partway thru lecture 41.

--- retro-calculator/ViewController.swift
+++ retro-calculator/ViewController.swift
@@ -7,17 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    var buttonSound: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundURL = NSURL(fileURLWithPath: path!)
+        do {
+            try buttonSound = AVAudioPlayer(contentsOfURL: soundURL)
+            buttonSound.prepareToPlay()  // get it buffered!
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func numberPressed(btn: UIButton!) {
+        buttonSound.play()
     }
 
 
